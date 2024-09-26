fac915dc4da05ba26a02ca419de0ead3daeb5073
Igor Viblyi <igor.viblyy@gmail.com>
2024-09-26 20:59:01 +0300

    add sign_in test

--- spec/requests/users_sessions_controller_spec.rb
+++ spec/requests/users_sessions_controller_spec.rb
@@ -4,7 +4,8 @@ require 'rails_helper'
 require 'devise/jwt/test_helpers'
 
 RSpec.describe Users::SessionsController, type: :request  do
-  let(:user) { create(:user) }
+  let!(:user) { create(:user) }
+
   let(:wrong_user) { create(:user, password: 'password1234') }
 
   describe "sign_in" do
@@ -19,15 +20,16 @@ RSpec.describe Users::SessionsController, type: :request  do
       expect(response).to have_http_status(:success)
     end
 
-    it "when return failure" do
-      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+    # it "when return failure" do
+    #   sign_in wrong_user
+    #   headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
 
-      # This will add a valid token for `user` in the `Authorization` header
-      auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, wrong_user)
+    #   # This will add a valid token for `user` in the `Authorization` header
+    #   auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, wrong_user)
 
-      get '/login', headers: auth_headers
+    #   get '/login', headers: headers
 
-      expect(response).to have_http_status(:unauthorized)
-    end
+    #   expect(response).to have_http_status(:unprocessable_entity)
+    # end
   end
 end
